f6e9ae750919083d73760747115633fd218d60e6
noscripter <noreply>
2026-05-18 14:51:21 +0800

    build(env):

--- env/entrypoint.sh
+++ env/entrypoint.sh
@@ -2,4 +2,4 @@
 cd /app
 python3 server.py &>/dev/null &
 # setarch -R disables ASLR for the launched process (deterministic addresses)
-exec setarch linux64 -R /nginx-src/build/nginx -p /app -c /app/nginx.conf
+exec setarch x86_64 -R /nginx-src/build/nginx -p /app -c /app/nginx.conf
