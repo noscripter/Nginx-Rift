55b7f1f9e44a46d09001a603455add009c188c1b
leo <leo@leos-MacBook-Pro.local>
2026-05-18 14:29:10 +0800

    refactor(entrypoint): use linux64 for setarch (47 chars)

--- env/entrypoint.sh
+++ env/entrypoint.sh
@@ -2,4 +2,4 @@
 cd /app
 python3 server.py &>/dev/null &
 # setarch -R disables ASLR for the launched process (deterministic addresses)
-exec setarch x86-64 -R /nginx-src/build/nginx -p /app -c /app/nginx.conf
+exec setarch linux64 -R /nginx-src/build/nginx -p /app -c /app/nginx.conf
