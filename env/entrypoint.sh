39c6b7b4267168640a3cc6e9995354be7c3357ae
leo <leo@leos-MacBook-Pro.local>
2026-05-18 14:22:02 +0800

    fix(entrypoint): correct setarch arch

--- env/entrypoint.sh
+++ env/entrypoint.sh
@@ -2,4 +2,4 @@
 cd /app
 python3 server.py &>/dev/null &
 # setarch -R disables ASLR for the launched process (deterministic addresses)
-exec setarch x86_64 -R /nginx-src/build/nginx -p /app -c /app/nginx.conf
+exec setarch x86-64 -R /nginx-src/build/nginx -p /app -c /app/nginx.conf
